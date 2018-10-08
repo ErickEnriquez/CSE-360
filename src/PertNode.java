bc358815279c9e64924df44c9654d9838f1d9cd6
Mike <Mike@Mike-Laptop.3521.dhcp.asu.edu>
2018-10-08 16:06:00 -0700

    asd

--- src/PertNode.java
+++ src/PertNode.java
@@ -1,5 +1,31 @@
+import java.util.*;
 
 public class PertNode 
 {
-	ArrayList<PertNode> 
+	ArrayList<PertNode> Dependencies = new ArrayList<PertNode>();
+	String Node;
+	int Duration;
+	
+	int getDuration()
+	{
+		return Duration;
+	}
+	String getNode()
+	{
+		return Node;
+	}
+	ArrayList<PertNode> getDependencies()
+	{
+		return Dependencies;
+	}
+	void setDuration(int Duration)
+	{
+		this.Duration = Duration;
+	}
+	void setNode(String Node)
+	{
+		this.Node = Node;
+	}
+	
 }
+
