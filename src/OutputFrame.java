76d4c30247ab2b71fb7fb6de179eee83d1144763
Erick Enriquez <eenriqu5@asu.edu>
2018-10-09 09:49:55 -0700

    added a help button on the output frame to help user understand output

--- src/OutputFrame.java
+++ src/OutputFrame.java
@@ -3,6 +3,11 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
 
 public class OutputFrame {
 
@@ -42,12 +47,23 @@ public class OutputFrame {
 		frame.getContentPane().setLayout(null);
 		
 		textField = new JTextField();
-		textField.setBounds(12, 24, 408, 216);
+		textField.setBounds(12, 72, 408, 168);
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
+		textField.setText("output will go here");
 		
 		JLabel criticalPathLabel = new JLabel("critical path");
-		criticalPathLabel.setBounds(149, 0, 109, 16);
+		criticalPathLabel.setForeground(Color.RED);
+		criticalPathLabel.setBounds(166, 0, 109, 59);
 		frame.getContentPane().add(criticalPathLabel);
+		
+		JButton helpButton = new JButton("Help");
+		helpButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				JOptionPane.showMessageDialog(null, "The output in this tab is the ciritical path of the network along with the total time it takes");
+			}
+		});
+		helpButton.setBounds(293, 34, 97, 25);
+		frame.getContentPane().add(helpButton);
 	}
 }
